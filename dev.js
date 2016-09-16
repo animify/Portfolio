fcb4fd5e0f87eb3d2f23b4830ff718d5e1faef98
Stefan Mansson <Aotik@Stefans-MacBook-Pro.local>
2016-09-16 20:36:43 +0300

    Portfolio
    Init

---
+++ dev.js
@@ -0,0 +1,5 @@
+const connect = require('connect')
+const serveStatic = require('serve-static')
+connect().use(serveStatic(__dirname)).listen(1337, function(){
+		console.log('Serving static files')
+})
---
+++ gruntfile.js
@@ -0,0 +1,29 @@
+module.exports = function(grunt) {
+
+	grunt.initConfig({
+		pug: {
+			compile: {
+				options: {
+					data: {
+						debug: false
+					}
+				},
+				files: {
+					'index.html': ['pug/*.pug']
+				}
+			}
+		},
+		watch: {
+			scripts: {
+				files: 'pug/*.pug',
+				tasks: ['default'],
+			}
+		}
+	});
+
+	grunt.loadNpmTasks('grunt-contrib-pug');
+	grunt.loadNpmTasks('grunt-contrib-watch');
+
+	grunt.registerTask('default', ['pug']);
+
+};
