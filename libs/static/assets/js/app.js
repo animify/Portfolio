02d21b1f488ee9f11fe3eef683f50573df5a56c6
Stefan Mansson <stefan.aotik@gmail.com>
2018-05-28 10:41:53 +0300

    Copy changes

--- libs/static/assets/js/app.js
+++ libs/static/assets/js/app.js
@@ -1,14 +1,14 @@
 setTimeout(() => {
-  $('#introduction .inner').animate({
-    height: $('#introduction .heading').height(),
-    marginTop: 0
-  }, 500);
+    $('#introduction .inner').animate({
+        height: $('#introduction .heading').height(),
+        marginTop: 0
+    }, 500);
 
-  $('#introduction').css({
-    height: 'auto'
-  });
+    $('#introduction').css({
+        height: 'auto'
+    });
 
-  $('#content').show().animate({
-    opacity: 1
-  }, 500);
+    $('#content').show().animate({
+        opacity: 1
+    }, 500);
 }, 1000);
