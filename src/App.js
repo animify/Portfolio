14b8117228d7cece4aea8d03512082887687b509
Stefan Mansson <st.mansson@icloud.com>
2018-11-14 21:52:59 +0200

    Update link URLs

--- src/App.js
+++ src/App.js
@@ -40,42 +40,42 @@ class App extends Component {
             <Project
               header="The Colorbook"
               desc="A timeline of the trendiest color palettes from Dribbble, as seen on Product Hunt."
-              url="https://github.com/animify/"
+              url="https://colorbook.me"
               urlText="Self started — colorbook.me"
               image="colorbook"
             />
             <Project
               header="Minicons"
               desc="Minicons are a charming, open-source SVG iconset, embeddable with Javascript."
-              url="https://github.com/animify/"
+              url="https://minicons.io"
               urlText="Self started — minicons.io"
               image="minicons"
             />
             <Project
               header="Blossom"
               desc="A building block system for the modern web."
-              url="https://github.com/animify/"
+              url="https://getblossom.io"
               urlText="Self started — getblossom.io"
               image="blossom"
             />
             <Project
               header="Unload"
               desc="A transitory code sharing platform for developers to distribute snippets rapidly."
-              url="https://github.com/animify/"
+              url="http://unload.io"
               urlText="Self started — unload.io"
               image="unload"
             />
             <Project
               header="Kiteshare"
               desc="Kiteshare is a screen capturing desktop tool for users to capture &amp; share moments in seconds."
-              url="https://github.com/animify/"
+              url="http://kiteshare.io"
               urlText="Self started — kiteshare.io"
               image="kiteshare"
             />
             <Project
               header="Slant.tv"
               desc="Slant makes searching for a movie or tv-series information effortless, powered by OMDB."
-              url="https://github.com/animify/"
+              url="http://slant.tv"
               urlText="Self started — slant.tv"
               image="slant"
             />
@@ -89,28 +89,28 @@ class App extends Component {
               date="Sep 2018"
               header="Switcher"
               desc="A super simple and modern way to change between dark &amp; light themes on sites, powered by CSS4 variables and JS."
-              url="https://github.com/animify/"
+              url="https://github.com/animify/Switcher"
               tags={["CSS4", "Javascript"]}
             />
             <SideProject
               date="Sep 2017"
               header="Webpack, React, Express"
               desc="A basic Webpack, React and Express boilerplate to get a quick client &amp; server project up and running."
-              url="https://github.com/animify/"
+              url="https://github.com/animify/webpack-react-express"
               tags={["CSS4", "Javascript"]}
             />
             <SideProject
               date="May 2017"
               header="Darken.io"
               desc="Content delivery network for Cindr.io and associated platforms, using the fastest content distribution methods available."
-              url="https://github.com/animify/"
+              url="https://github.com/humanstack/Darken"
               tags={["CSS4", "Javascript"]}
             />
             <SideProject
               date="Apr 2017"
               header="Blossom boilerplate"
               desc="Set up static websites styled by Stylus, templated by PugJS and utilizing Browser Sync and Gulp for development."
-              url="https://github.com/animify/"
+              url="https://github.com/animify/blossom-boilerplate"
               tags={["CSS4", "Javascript"]}
             />
           </div>
