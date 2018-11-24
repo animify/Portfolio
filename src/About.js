e0fff507a6b7b47937705752698eb119f6cd9640
Stefan Mansson <stefan.aotik@gmail.com>
2018-11-24 19:37:09 +0200

    About update

--- src/About.js
+++ src/About.js
@@ -8,7 +8,7 @@ const About = () => (
         I'm Stefan, a 23 year old Software Engineer based out of Nicosia, Cyprus. I currently work at Overflow where I'm engineering the next generation user flow diagramming platform for designers.
       </h3>
       <h3>In my spare time, I primarily focus on UI/UX design. Having no formal design education, I've been studying tons of resources and projects over the past few years that gives me an edge when starting new projects or jumping in to more complex projects.</h3>
-      <h3>Good engineering is tough. Great engineering is invisible. Same goes for design. </h3>
+      <h3>Good engineering is tough. Great engineering is invisible. Ditto for design.</h3>
     </div>
     <div className="mentions">
       <h2>Mentions around the interwebs</h2>
