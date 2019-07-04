0416313fafb878b2d8ec5ab32cc65d2b255ff7f2
Stefan Mansson <stefan.aotik@gmail.com>
2019-07-04 19:51:48 +0300

    chore: tslint and prettier configs

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,17 @@ import "./styles/app.scss";
 import Project from "./components/Project";
 
 const App: React.FC = () => {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <Project
-          title="Blossom"
-          description="A building block, design framework for the modern web."
-          mode="light"
-        />
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <header className="App-header">
+                <Project
+                    title="Blossom"
+                    description="A building block, design framework for the modern web."
+                    mode="light"
+                />
+            </header>
+        </div>
+    );
 };
 
 export default App;
--- src/components/Project.tsx
+++ src/components/Project.tsx
@@ -1,24 +1,24 @@
-import React from "react";
-import classNames from "classnames";
+import React from 'react';
+import classNames from 'classnames';
 
 interface IProps {
-  mode: "light" | "dark";
-  title: string;
-  description: string;
-  image?: string;
+    mode: 'light' | 'dark';
+    title: string;
+    description: string;
+    image?: string;
 }
 
 export default function Project({ mode, title, description }: IProps) {
-  const classes = classNames("project", mode);
+    const classes = classNames('project', mode);
 
-  return (
-    <div className={classes}>
-      <div className="flex justify-between">
-        <div className="info">
-          <h5>{title}</h5>
-          <p className="mt2">{description}</p>
+    return (
+        <div className={classes}>
+            <div className="flex justify-between">
+                <div className="info">
+                    <h5>{title}</h5>
+                    <p className="mt2">{description}</p>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
