57c49a4cf5a77f1fe3d1683527deca9627d71b76
Stefan Mansson <stefan.aotik@gmail.com>
2019-07-04 23:42:02 +0300

    feat: menu animation

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,16 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import classNames from 'classnames';
 
 export default function Nav() {
     const [menuOpen, setMenuOpen] = useState(false);
-    const menuClasses = classNames('menu', { open: menuOpen });
+    const toggleClasses = classNames('toggle', { open: menuOpen });
+    const navClasses = classNames({ open: menuOpen });
+    const options = [{ title: 'About me' }, { title: 'Resume' }, { title: "Let's chat" }];
+
+    useEffect(() => {
+        if (menuOpen) {
+            document.body.classList.add('nooverflow');
+
+            window.scroll({
+                top: 0,
+                left: 0,
+                behavior: 'smooth',
+            });
+        } else {
+            document.body.classList.remove('nooverflow');
+        }
+    }, [menuOpen]);
 
     return (
-        <nav className="flex justify-between">
-            <a className="logo" href="">
-                Hi, I'm Stefan.
-            </a>
-            <div className={menuClasses} onClick={() => setMenuOpen(!menuOpen)} />
+        <nav className={navClasses}>
+            <div className="menu">
+                <div className="inner flex flex-column">
+                    <p className="split">Contents</p>
+
+                    {options.map((option, index) => (
+                        <a href="" style={{ transitionDelay: `${200 * (index + 1) + 240}ms` }}>
+                            {option.title}
+                        </a>
+                    ))}
+                </div>
+            </div>
+            <div className="inner flex justify-between">
+                <a className="logo" href="">
+                    Hi, I'm Stefan.
+                </a>
+                <div>
+                    <div className={toggleClasses} onClick={() => setMenuOpen(!menuOpen)} />
+                </div>
+            </div>
         </nav>
     );
 }
