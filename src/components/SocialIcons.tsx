5267258ec1ad9513587edd27d7f5d03a780e710a
Stefan Mansson <stefan.aotik@gmail.com>
2019-08-01 20:25:53 +0300

    feat: Social icon links

--- src/components/SocialIcons.tsx
+++ src/components/SocialIcons.tsx
@@ -4,16 +4,16 @@ import { Icon } from 'figicons';
 export default function SocialIcons() {
     return (
         <div className="social">
-            <a href="">
+            <a href="https://dribbble.com/animify">
                 <Icon name="dribbble" fill="currentColor" strokeWidth={0} />
             </a>
-            <a href="">
+            <a href="https://twitter.com/_animify">
                 <Icon name="twitter" fill="currentColor" strokeWidth={0} />
             </a>
-            <a href="">
+            <a href="https://github.com/animify">
                 <Icon name="github" fill="currentColor" strokeWidth={0} />
             </a>
-            <a href="">
+            <a href="https://www.linkedin.com/in/smansson">
                 <Icon name="linkedin" fill="currentColor" strokeWidth={0} />
             </a>
         </div>
