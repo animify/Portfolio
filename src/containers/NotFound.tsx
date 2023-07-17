a0831066161580ae1a663b8cac3fb96b04967cef
Stefan Mansson <st.mansson@icloud.com>
2023-07-17 19:55:03 +0300

    clean

--- src/containers/NotFound.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function NotFound() {
-    return (
-        <div className="container tc">
-            <p>This isn't what you're looking for.</p>
-        </div>
-    );
-}
--- src/react-router-ga.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'react-router-ga' {
-    const x: any;
-    export = x;
-}
